94b3b02ca52aed6641dba69a4c68aee8a4463161
Kyle Wolfe <Kyle Wolfe>
2016-06-16 16:56:23 -0400

    BT37 Clamping and Rotating Tank Barrel

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -5,7 +5,16 @@
 
 void UTankBarrel::MoveTo(float Pitch)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%f"), Pitch);
+	// clamping and scaling the (target - previous) barrel rotation to maximum rotation speed
+	float ElevationChange = FMath::Clamp((Pitch - RelativeRotation.Pitch), -MaxDegreesPerSecond, MaxDegreesPerSecond);
+	ElevationChange *= GetWorld()->DeltaTimeSeconds;
+
+	// update and clamp the new pitch target rotation
+	float RawNewPitch = RelativeRotation.Pitch + ElevationChange;
+	RawNewPitch = FMath::Clamp(RawNewPitch, MinDegreesElevation, MaxDegreesElevation);
+
+	// rotating the barrel to the new target rotation
+	SetRelativeRotation(FRotator(RawNewPitch, 0, 0));
 }
 
 
