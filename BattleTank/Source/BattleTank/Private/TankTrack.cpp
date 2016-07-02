509f0d933eff58f79ce7d8779f8976c22686b6f8
Kyle Wolfe <Kyle Wolfe>
2016-07-02 12:18:25 -0400

    BT51 SetThrottle Behavior and Inputs

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,5 +4,11 @@
 #include "TankTrack.h"
 
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle Value: %f"), *Name, Throttle);
 
+	// TODO clamp actual throttle value so player can't overdrive
+}
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -13,7 +13,9 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
 	
 	
 };
