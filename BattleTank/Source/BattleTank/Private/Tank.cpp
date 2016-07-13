95a13998930f51b4b6ed552f1c164eb9f289a70e
Kyle Wolfe <Kyle Wolfe>
2016-07-13 16:06:15 -0400

    BT66 More bug investigation

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -12,6 +12,15 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Warning, TEXT("Tank Constructor C++"));
+}
+
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Tank Begin Play C++"));
 }
 
 void ATank::AimAt(FVector HitLocation)
